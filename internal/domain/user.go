08dbb206f6ea243adc372e1ba74dbd248a698f81
LucasLCabral <lucasludovicocabraal@gmail.com>
2025-08-12 08:56:07 -0300

    feat: implementing user domain

---
+++ internal/domain/user.go
@@ -0,0 +1,33 @@
+package domain
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"github.com/LucasLCabral/go-api/pkg/domain"
+)
+
+type User struct {
+	ID       domain.ID `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+}
+
+func NewUser(id, name, email, password string) (*User, error ){
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:       domain.NewID(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
+}
+
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
+
