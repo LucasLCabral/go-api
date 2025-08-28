4048b842810d5a9b67348de87c10c8d059b3c1f2
LucasLCabral <lucasludovicocabraal@gmail.com>
2025-08-28 09:00:23 -0300

    feat: implemented TestFindByEmail

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -27,3 +27,23 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.NotNil(t, "password123", userFound.Password)
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("failed to connect database: %v", err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Lucas zizi", "zizi@email.com", "password123")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, "password123", userFound.Password)
+}
