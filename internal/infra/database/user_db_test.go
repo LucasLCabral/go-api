f53949fab7e4bfe4e9f8295044804677f919fc18
LucasLCabral <lucasludovicocabraal@gmail.com>
2025-08-27 23:11:07 -0300

    feat: implemented user db test

---
+++ internal/infra/database/user_db_test.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/LucasLCabral/go-api/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("failed to connect database: %v", err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Lucas zizi", "zizi@email.com", "password123")
+	UserDB := NewUser(db)
+	err = UserDB.Create(user)
+	assert.Nil(t, err)
+
+	var userFound entity.User
+	err = db.First(&userFound, "id = ?", user.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, "password123", userFound.Password)
+}
